54fe0fc2f07064ba1d295f4a8a0cab2b695e04aa
Sam Sheets <sam.sheets94@gmail.com>
2020-11-18 16:01:25 -0700

    display coordinates in order to select them as parameters for Storm Glass

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 'use strict';
 
-/*** Location Section ***/
+/*** API Keys & Base URLS ***/
 
-//variable to hold apiKey
+//variable to hold apiKeys
 const apiKeyOpenCage = '186aeff2de054932ad6fc90fa36c6c37';
+const apiKeyStormGlass = '';
 
-//variable to hold base url
+//variable to hold base urls
 const baseURLOpenCage = 'https://api.opencagedata.com/geocode/v1/json';
+const baseWeatherURLStormGlass = 'https://api.stormglass.io/v2/weather/point';
+const baseTideURLStormGlass = 'https://api.stormglass.io/v2/tide/extremes/point';
+const baseAstronomyURLStormGlass = 'https://api.stormglass.io/v2/astronomy/point';
 
-//format query params
+
+/*** Helper Functions ***/
+
+//function to format query params
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
   }
+
+
+/*** Location Section ***/
+
+//display lat/lng results
+function displayCoordinates(responseJson) {
+
+    console.log(responseJson);
+    $('.coordinates').empty();
+
+    $('.lat').text(responseJson.results[0].geometry.lat);
+    
+    
+    $('.lng').text(responseJson.results[0].geometry.lng);
+}
 
 //function to get lat/long coordinates in console.log (for errors, make sure to unhide <h4> error message)
 function getCoordinates(location) {
@@ -30,7 +52,7 @@ function getCoordinates(location) {
 
     fetch(urlOpenCage)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayCoordinates(responseJson))
         .catch(error => {
             $('.invalid-message').removeAttr('hidden');
         });
@@ -52,17 +74,42 @@ function watchSearchButton() {
 }
 
 
-/*** Results Section ***/
+/*** Results Section Functions ***/
 
-//variable to hold apiKey
 
-//variable to hold base url
+//function to display weather results
+function displayWeatherData() {
 
-//function to format query params
-
-//function to display results
+}
 
 //function to get weather data
+function getWeatherData(responseJson) {
+    const lat = responseJson.results[0].geometry.lat;
+    //const lng = responseJson.data.results[0].geometry.lng;
+
+    console.log(lat);
+    //console.log(lng);
+}
+
+//function to display tide data
+function displayTideData() {
+
+}
+
+//function to get tide data
+function getTideData() {
+
+}
+
+//function to display astronomy data
+function displayAstronomyData() {
+
+}
+
+//function to get astronomy data
+function getAstronomyData() {
+
+}
 
 
 /*** Document Ready ***/
