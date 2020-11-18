9c79f4a47d4d631eb18ceb1161574d552e33ddaa
Sam Sheets <sam.sheets94@gmail.com>
2020-11-18 12:17:06 -0700

    pseudocoding js file & added hidden html elements

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/*** Location Section ***/
+
+//variable to hold apiKey
+
+//variable to hold base url
+
+//function to get lat/long coordinates in console.log (for errors, make sure to unhide <h4> error message)
+
+//function to handle search input
+
+
+/*** Results Section ***/
+
+//variable to hold apiKey
+
+//variable to hold base url
+
+//function to format query params
+
+//function to display results
+
+//function to get weather data
+
+
+/*** Document Ready ***/
+
+//document ready
+$()
